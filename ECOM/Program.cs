e5567fba18cf7b7cbc8871a71f60a69fe9ff90bf
Dungbx <email@example.com>
2026-07-16 10:07:01 +0700

    Remove commented OpenAPI configuration.

--- ECOM/Program.cs
+++ ECOM/Program.cs
@@ -22,14 +22,6 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
-
-
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.MapOpenApi();
-// }
-
 app.MapControllers();
 // app.UseAuthentication();
 // app.UseAuthorization();
